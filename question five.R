b2813785050297081935010012487bbebef69e59
Abinah <abnergonyi@gmail.com>
2015-12-06 23:24:36 +0300

    qsn four

---
+++ question five.R
@@ -0,0 +1,20 @@
+# the top 100 things owned by the livng
+library(data.table)
+library(ggplot2)
+#load data
+living_things=read.csv("living-things-ii.csv",sep=";",header=FALSE)
+names(living_things)=c("person_id","item_id")
+#get how many times each thing is owned
+thing_table=as.data.table(table(living_things$item_id))
+#order the items from largest to smallest
+things=thing_table[order(-rank(thing_table$N))]
+#get the top 100 things
+#subset the data get only the top 100 rows
+top100things2=things[1:100]
+#so the top 100 things owned by the dead are
+top100things2[,top100things2$V1]
+
+#visualize them on a bar plot
+plot= qplot(x=V1, y=N,main="top100 owned things owned by the living", fill=V1,
+            data=top100things2, geom="bar",stat="identity",
+            position="dodge")
